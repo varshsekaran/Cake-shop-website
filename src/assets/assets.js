62f00e1b05796315542d1764eb6b655e31318a28
varsha <152765381+varshsekaran@users.noreply.github.com>
2025-01-31 22:23:21 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,350 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.jpg'
+import header_img from './header_img.jpg'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Truffle",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Forest Cake",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Brownie",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Cup Cake",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Caramel Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Cookies",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Jar Cake",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Plum Cake",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Choco Truffle",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Truffle"
+    },
+    {
+        _id: "2",
+        name: "Whitechoco Truffle",
+        image: food_2,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Truffle"
+    }, {
+        _id: "3",
+        name: "Nutty Truffle",
+        image: food_3,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Truffle"
+    }, {
+        _id: "4",
+        name: "Fruit Truffle",
+        image: food_4,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Truffle"
+    }, {
+        _id: "5",
+        name: "Black Forest",
+        image: food_5,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Forest Cake"
+    }, {
+        _id: "6",
+        name: "White Forest",
+        image: food_6,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Forest Cake"
+    }, {
+        _id: "7",
+        name: "Red Forest",
+        image: food_7,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Forest Cake"
+    }, {
+        _id: "8",
+        name: "Berry Forest",
+        image: food_8,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Forest Cake"
+    }, {
+        _id: "9",
+        name: "Classic Brownie",
+        image: food_9,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Brownie"
+    }, {
+        _id: "10",
+        name: "Cheesy Brownie",
+        image: food_10,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Brownie"
+    }, {
+        _id: "11",
+        name: "Caramel Brownie",
+        image: food_11,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Brownie"
+    }, {
+        _id: "12",
+        name: "Nut Brownie",
+        image: food_12,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Brownie"
+    },
+    {
+        _id: "13",
+        name: "Choco Cupcake",
+        image: food_13,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cup Cake"
+    },
+    {
+        _id: "14",
+        name: "Strawberry Cupcake",
+        image: food_14,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cup Cake"
+    }, {
+        _id: "15",
+        name: "Coconut Cupcake",
+        image: food_15,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cup Cake"
+    }, {
+        _id: "16",
+        name: "Berry Cupcake",
+        image: food_16,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cup Cake"
+    }, {
+        _id: "17",
+        name: "Coffee Caramel",
+        image: food_17,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Caramel Cake"
+    }, {
+        _id: "18",
+        name: "Cheesy Caramel",
+        image: food_18,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Caramel Cake"
+    }, {
+        _id: "19",
+        name: "Apple Caramel",
+        image: food_19,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Caramel Cake"
+    }, {
+        _id: "20",
+        name: "Salted Caramel",
+        image: food_20,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Caramel Cake"
+    }, {
+        _id: "21",
+        name: "Choco Cookies",
+        image: food_21,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cookies"
+    }, {
+        _id: "22",
+        name: "Fruity Cookies",
+        image: food_22,
+        price: 22,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cookies"
+    }, {
+        _id: "23",
+        name: "Coconut Cookies",
+        image: food_23,
+        price: 10,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cookies"
+    }, {
+        _id: "24",
+        name: "Spice Cookies",
+        image: food_24,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Cookies"
+    },
+    {
+        _id: "25",
+        name: "Chocolate Jar",
+        image: food_25,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Jar Cake"
+    },
+    {
+        _id: "26",
+        name: "Mangoe Jar",
+        image: food_26,
+        price: 18,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Jar Cake"
+    }, {
+        _id: "27",
+        name: "Strawberry Jar",
+        image: food_27,
+        price: 16,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Jar Cake"
+    }, {
+        _id: "28",
+        name: "Nutty Jar",
+        image: food_28,
+        price: 24,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Jar Cake"
+    }, {
+        _id: "29",
+        name: "Classic Plumcake",
+        image: food_29,
+        price: 14,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Plum Cake"
+    }, {
+        _id: "30",
+        name: "Fruity Plumcake",
+        image: food_30,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Plum Cake"
+    }, {
+        _id: "31",
+        name: "Mini Plumcake",
+        image: food_31,
+        price: 20,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Plum Cake"
+    }, {
+        _id: "32",
+        name: "Eggless Plumcake",
+        image: food_32,
+        price: 15,
+        description: "Food provides essential nutrients for overall health and well-being",
+        category: "Plum Cake"
+    }
+]
